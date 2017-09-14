f924dbc9c1e18235cd4dca981a39e64b976711a9
Felix Rieseberg <felix@felixrieseberg.com>
2017-09-15 07:42:00 +0900

    :wrench: Simple test

---
+++ src/view.js
@@ -0,0 +1,47 @@
+const { remote } = require('electron')
+const { Menu } = remote
+
+const template = [{
+  label: 'Edit',
+  submenu: [
+    {
+      role: 'undo'
+    },
+    {
+      role: 'redo'
+    },
+    {
+      type: 'separator'
+    },
+    {
+      role: 'cut'
+    },
+    {
+      role: 'copy'
+    },
+    {
+      role: 'paste'
+    },
+    {
+      role: 'pasteandmatchstyle'
+    },
+    {
+      role: 'delete'
+    },
+    {
+      role: 'selectall'
+    }
+  ]
+}]
+
+window.addEventListener('contextmenu', function(e) {
+  // Only show the context menu in text editors.
+  var menu = Menu.buildFromTemplate(template)
+
+  // The 'contextmenu' event is emitted after 'selectionchange' has fired but possibly before the
+  // visible selection has changed. Try to wait to show the menu until after that, otherwise the
+  // visible selection will update after the menu dismisses and look weird.
+  setTimeout(function() {
+    menu.popup(remote.getCurrentWindow());
+  }, 30);
+});
